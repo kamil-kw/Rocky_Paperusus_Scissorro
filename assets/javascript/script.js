03953b2167ad125b74147da8fb74432e04880c87
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-08-13 10:11:26 +0000

    Add fighterComparation function to define winner

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,3 +13,23 @@ function chooseFighter(userFighter) {
     console.log('user', userFighter);
     console.log('computer', computerFighter);
 }
+
+/** Compare characters - Fight :) */
+
+function fighterComparation(user, computer) {
+    if(user === 'rocky') {
+        if(computer === 'paperusus') return 'win';
+        if(computer === "scissorro") return 'loose';
+        if(computer === "rocky") return 'draw';
+    }
+    if(user == 'scissorro') {
+        if(computer === 'paperusus') return 'win';
+        if(computer === "rocky") return 'loose';
+        if(computer === "rscissorro") return 'draw';
+    }
+    if(user == 'scissorro') {
+        if(computer === 'paperusus') return 'win';
+        if(computer === "rocky") return 'loose';
+        if(computer === "rscissorro") return 'draw';
+}
+}
