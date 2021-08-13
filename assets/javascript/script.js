044df7243f12af9f27a8eeb7ede87e0a01567de9
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-08-13 13:50:59 +0000

    Add score calculation for user and computer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,28 +3,47 @@
  * */
 function chooseFighter(userFighter) {
     /** declare array */
-    const fighterCharacter=['rocky', 'paperusus','scissorro'];
+    const fighterCharacter=['rocky', 'paperusus', 'scissorro'];
 
     /** create random number to choose compuerFighter*/
     const randomNumber = Math.random()*fighterCharacter.length;
     const roundNumber = Math.ceil(randomNumber)-1;
     const computerFighter = fighterCharacter[roundNumber];
-    
     const winner = fighterComparation(userFighter, computerFighter);
+    let resultUser = '';
+    let resultComputer ='';
 
     showWinner(winner);
 
-    function showWinner(winner) {
-        let result = '';
-        if (winner == 'draw') result = "DRAW!";
-        if (winner == 'win') result = "WINNER";
-        if (winner == 'loose') result = "LOOSER";
-        document.getElementById('game--result').innerHTML = result;
+    function showWinner() {
+
+        if (winner === 'loose') {
+            resultUser = "LOOSER";
+            resultComputer = "WINNER";
+
+        } else if (winner === 'win'){
+            resultUser = "WINNER";
+            resultComputer = "LOOSER";
+
+        } else if(winner === 'draw'){
+            resultUser = "DRAW";
+            resultComputer = "DRAW";
+        }
+    
+        document.getElementById('user--result').innerHTML = resultUser;
+        document.getElementById('computer--result').innerHTML = resultComputer;
 
         let userChoosed = document.getElementById('player--choice');
         let computerChoosed = document.getElementById('computer--choice');
         userChoosed.className = `fighter fighter--${userFighter}`;
         computerChoosed.className = `fighter fighter--${computerFighter}`;
+    }
+    if (resultUser === "WINNER") {
+        let playerScore = parseInt(document.getElementById("player--scored").innerText);
+        document.getElementById("player--scored").innerText = ++playerScore;
+    } else if(resultComputer === "WINNER") {
+        let computerScore = parseInt(document.getElementById("computer--scored").innerText);
+        document.getElementById("computer--scored").innerText = ++computerScore;
     }
 }
 
